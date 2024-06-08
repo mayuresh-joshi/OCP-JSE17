c3682c63c4d573bbd35996c880e248f7346376d1
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-06-08 19:09:23 +0530

    Testing related to promotions

---
+++ src/main/java/org/mj/module1/lesson1/MyTesting.java
@@ -0,0 +1,15 @@
+package org.mj.module1.lesson1;
+
+public class MyTesting {
+    public static void main(String[] args) {
+        byte b = 1;
+        short s = 1;
+        byte b2 = 1;
+        short i1 = 1;
+        short i2 = 2;
+
+        //short z = b + s;
+        //byte b3 = b + b2;
+        //short i3 = i1 + i2;
+    }
+}

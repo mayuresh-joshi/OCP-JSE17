c7629237c8ea9a8d56dd1dae6cb88d2eefc4bdc5
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-06-27 23:10:03 +0530

    Some revision

--- src/main/java/org/mj/module3/lesson8/InstanceAndStaticFields.java
+++ src/main/java/org/mj/module3/lesson8/InstanceAndStaticFields.java
@@ -1,13 +1,26 @@
 package org.mj.module3.lesson8;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class InstanceAndStaticFields {
 
-    // order or modifiers does not matter, unless governed by convention
+    // Comma separated list allowed for fields of SAME type
+    HashMap hs = new HashMap(), hs2, hs3 = new HashMap();
+
+    // Array declarations
+    int[] ia = new int[]{1, 2, 3, 4, 5}; // Preferred style
+    int[] ia2 = {1, 2, 3, 4, 5};
+    int ia3[] = {1, 2, 3, 4, 5}; // Permitted only because of C++ devs
+
+    int[] ia4 [] = new int[][]{{1, 2, 3}, {4, 5, 6}}; // Ugly but legal
+    int [] ia5[] = {{1, 2, 3}, {4, 5, 6}}; // Ugly but legal. also spaces are ignored in array declaration between 'type' and '[]'
+
+    // Order of modifiers does not matter, unless governed by convention
     public static final @Deprecated List<String> ls = new ArrayList<>();
-    final static  @Deprecated public List<String> ls2 = new ArrayList<>();
+    final static @Deprecated
+    public List<String> ls2 = new ArrayList<>();
 
     // final fields must be initialized (either directly or in static/instance initializer), else compilation fails
     final static public @Deprecated List<String> ls3;
@@ -21,6 +34,7 @@ public class InstanceAndStaticFields {
     {
         ls4 = new ArrayList<>();
     }
+
     public static void main(String[] args) {
 
     }

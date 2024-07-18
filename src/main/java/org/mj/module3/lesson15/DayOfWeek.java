db065021eaa781742c6d9950c4c5cb812ff27f98
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-07-18 22:21:44 +0530

    Lesson 16 start

--- src/main/java/org/mj/module3/lesson15/DayOfWeek.java
+++ src/main/java/org/mj/module3/lesson15/DayOfWeek.java
@@ -1,13 +1,22 @@
 package org.mj.module3.lesson15;
 
 enum DayOfWeek {
-    SUNDAY(),MONDAY("Back to work!"), TUESDAY;
+    SUNDAY(), MONDAY("Back to work!"), TUESDAY;
 
-    DayOfWeek(String s) {
+    // Instance and Static fields allowed in enum
+    public Integer a = 1;
+    static public Integer aStatic = 1;
+
+    // Constructors are private by default
+    /*public*/ DayOfWeek(String s) {
     }
 
     DayOfWeek() {
 
+    }
+
+    // Instance and Static methods allowed in enum
+    void doSomething() {
     }
 
     public static void main(String[] args) {
--- src/main/java/org/mj/module4/lesson16/Generics.java
+++ src/main/java/org/mj/module4/lesson16/Generics.java
@@ -1,4 +1,13 @@
 package org.mj.module4.lesson16;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Generics {
+    public static void main(String[] args) {
+
+        List<String> listString = new ArrayList<>(/*List.of(LocalDate.of(1997, 9, 19))*/);
+        List<String> listString2 = new ArrayList<>(List.<String>of("Mj")); // new for me but allowed
+    }
 }

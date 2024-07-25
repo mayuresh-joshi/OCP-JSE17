10a089bf6b5e4db41216455f2f22b53f186f3602
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-07-25 21:08:50 +0530

    Lesson 17 day 2

--- src/main/java/org/mj/module4/lesson17/ArraysExamples.java
+++ src/main/java/org/mj/module4/lesson17/ArraysExamples.java
@@ -4,7 +4,14 @@ public class ArraysExamples {
     public static void main(String[] args) {
 
         int[] ai = new int[5]; // needs size/list of elements else comp fail
-        int ia2[] = new int[5]; // depricated but allowed
+        int ia2[] = new int[5]; // deprecated but allowed
+
+        String[][] si = new String[5][]; // partial create
+        System.out.println(si[1]);
+        si[1] = new String[5];
+        System.out.println(si[1]);
+
+        System.out.println(ai[2]);
         String[] sa = {,};
         System.out.println(sa.length);
         String[] sa2 = new String[]{"MJ",};
---
+++ src/main/java/org/mj/module4/lesson17/ArraysUtilityMethods.java
@@ -0,0 +1,7 @@
+package org.mj.module4.lesson17;
+
+public class ArraysUtilityMethods {
+    public static void main(String[] args) {
+        System.out.println("Practice all Arrays.xxx() methods");
+    }
+}

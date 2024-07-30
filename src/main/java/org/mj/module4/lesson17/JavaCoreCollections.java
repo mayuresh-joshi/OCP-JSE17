28a9f0f91812d4805db3fef8a3fb5bc1414e3faa
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-07-30 21:55:54 +0530

    Lesson 17 day 3

--- src/main/java/org/mj/module4/lesson17/JavaCoreCollections.java
+++ src/main/java/org/mj/module4/lesson17/JavaCoreCollections.java
@@ -2,6 +2,6 @@ package org.mj.module4.lesson17;
 
 public class JavaCoreCollections {
     public static void main(String[] args) {
-
+        System.out.println("Lesson 17 continues...");
     }
 }

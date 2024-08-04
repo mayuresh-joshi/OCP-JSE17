2ba6a5928283435a6f9deafbf916677365882cd9
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-08-04 20:20:42 +0530

    Lessoon 19 Start and End

--- src/main/java/org/mj/module5/lesson19/LambdaExpressions.java
+++ src/main/java/org/mj/module5/lesson19/LambdaExpressions.java
@@ -1,7 +1,13 @@
 package org.mj.module5.lesson19;
 
+import java.util.function.Predicate;
+
 public class LambdaExpressions {
     public static void main(String[] args) {
+        Predicate<String> prd = ((Predicate<String>) s -> s.length() > 3)
+                .and(s -> s.startsWith("M"));
 
+        System.out.println(prd.test("MJ"));
+        System.out.println(prd.test("Mayuresh"));
     }
 }

abb0e4c93f889dce8ffef1133897543df59a5b46
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-08-18 17:54:45 +0530

    Lesson 24 ends

---
+++ src/main/java/org/mj/module6/lesson24/ProjectMigration.java
@@ -0,0 +1,7 @@
+package org.mj.module6.lesson24;
+
+public class ProjectMigration {
+    public static void main(String[] args) {
+        System.out.println("Done...");
+    }
+}
---
+++ src/main/java/org/mj/module7/lesson25/CreateAndExecuteThreads.java
@@ -0,0 +1,7 @@
+package org.mj.module7.lesson25;
+
+public class CreateAndExecuteThreads {
+    public static void main(String[] args) {
+        System.out.println("Concurrency and Parallelism is different...");
+    }
+}

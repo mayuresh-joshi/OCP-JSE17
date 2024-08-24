7687b433832c9d48ed43a6c7fd4da46004e93aec
Mayuresh Joshi <mayuresh_joshi@persistent.com>
2024-08-24 12:31:41 +0530

    Lesson 25 end

---
+++ src/main/java/org/mj/module7/lesson26/ThreadSafeCodeLockingAndSynchronization.java
@@ -0,0 +1,7 @@
+package org.mj.module7.lesson26;
+
+public class ThreadSafeCodeLockingAndSynchronization {
+    public static void main(String[] args) {
+        System.out.println("Avoiding Deadlocks...");
+    }
+}

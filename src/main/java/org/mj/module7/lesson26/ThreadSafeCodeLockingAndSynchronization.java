2b00f7fe003d7c6e2c195481d85760f7f027d012
Mayuresh Joshi <mayureshjoshee3@gmail.com>
2024-08-29 12:52:47 +0000

    Lesson 26 end

--- src/main/java/org/mj/module7/lesson26/ThreadSafeCodeLockingAndSynchronization.java
+++ src/main/java/org/mj/module7/lesson26/ThreadSafeCodeLockingAndSynchronization.java
@@ -2,6 +2,6 @@ package org.mj.module7.lesson26;
 
 public class ThreadSafeCodeLockingAndSynchronization {
     public static void main(String[] args) {
-        System.out.println("Avoiding Deadlocks...");
+        System.out.println("Avoiding Deadlocks, Livelocks and Race conditions...");
     }
 }

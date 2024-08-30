84acfd8c5de34906ddc48e8a1dffb422703ed089
Mayuresh Joshi <mayureshjoshee3@gmail.com>
2024-08-30 10:59:33 +0000

    Lesson 27 on hold and Lesson 28 end

---
+++ src/main/java/org/mj/module8/lesson27/JDBC.java
@@ -0,0 +1,7 @@
+package org.mj.module8.lesson27;
+
+public class JDBC {
+    public static void main(String[] args) {
+        System.out.println("On hold for now.");
+    }
+}
---
+++ src/main/java/org/mj/module8/lesson28/JavaIO.java
@@ -0,0 +1,7 @@
+package org.mj.module8.lesson28;
+
+public class JavaIO {
+    public static void main(String[] args) {
+        System.out.println("Done....");
+    }
+}

bf32dbb55c890bdc97a7b92f55c2311d5b20dcaf
Mayuresh Joshi <mayureshjoshee3@gmail.com>
2024-09-02 10:49:46 +0000

    Lesson 30 and 31 ends

---
+++ src/main/java/org/mj/module9/lesson31/LocalizationInJava.java
@@ -0,0 +1,9 @@
+package org.mj.module9.lesson31;
+
+import java.util.Locale;
+
+public class LocalizationInJava {
+    public static void main(String[] args) {
+        System.out.println("Current Locale: "+ Locale.getDefault());
+    }
+}

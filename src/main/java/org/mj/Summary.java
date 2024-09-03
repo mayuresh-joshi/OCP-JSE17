ca01dc28bea5188313b1e463620ea8b4ba476ac1
Mayuresh Joshi <mayureshjoshee3@gmail.com>
2024-09-03 11:22:00 +0000

    Course Done!

---
+++ src/main/java/org/mj/Summary.java
@@ -0,0 +1,16 @@
+package org.mj;
+
+public class Summary {
+    public static void main(String[] args) {
+        String summaryPoints= """
+                1. Study carefully.
+                
+                2. Keep practicing even after certification.
+                
+                3. You can never know a language 'too well' in a professional situation.
+                
+                4. Cheers!""";
+
+        System.out.println(summaryPoints);
+    }
+}
--- src/main/java/org/mj/module8/lesson27/JDBC.java
+++ src/main/java/org/mj/module8/lesson27/JDBC.java
@@ -2,6 +2,6 @@ package org.mj.module8.lesson27;
 
 public class JDBC {
     public static void main(String[] args) {
-        System.out.println("On hold for now.");
+        System.out.println("Done...");
     }
 }

8d4e54fd93ac043630acbfdb9e6137c33fdc4882
Mayuresh Joshi <mayureshjoshee3@gmail.com>
2024-10-18 10:17:37 +0000

    Simple Web Server Example

---
+++ src/main/java/org/mj/from17to21/JEP408_simplewebserver/Example.java
@@ -0,0 +1,21 @@
+package org.mj.from17to21.JEP408_simplewebserver;
+
+import com.sun.net.httpserver.SimpleFileServer;
+
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+
+public class Example {
+
+    public static void main(String[] args) {
+        startFileServer();
+        System.out.println("Go to -> http://localhost:8080/");
+    }
+
+    private static void startFileServer() {
+        var server = SimpleFileServer.createFileServer(new InetSocketAddress(8080),
+                Path.of("D:\\git\\OCP-JSE17\\src\\main\\java\\org\\mj\\from17to21\\JEP408_simplewebserver"),
+                SimpleFileServer.OutputLevel.VERBOSE);
+        server.start();
+    }
+}

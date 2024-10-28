032bd01b49589d55bd7d2504712888386dd1b11c
Mayuresh Joshi <mayureshjoshee3@gmail.com>
2024-10-28 12:37:12 +0000

    Syllabus completed

--- src/main/java/org/mj/Main.java
+++ src/main/java/org/mj/Main.java
@@ -3,5 +3,6 @@ package org.mj;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello OCP-JSE17 world!");
+        System.out.println("Syllabus completed on 28/10/2024");
     }
 }
